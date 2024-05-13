bb7672f84835006761ca4e36d5f9fe1708156b01
Ati <ativassileva@gmail.com>
2024-05-13 14:33:51 +0300

    Added posts controller

---
+++ NonHateSpeechForum/NonHateSpeechForum/Controllers/PostsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NonHateSpeechForum.Controllers
+{
+    public class PostsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PostsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

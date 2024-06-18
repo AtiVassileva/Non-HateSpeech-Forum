089eaa5c1d526dca5f127fe104dd323b563c5541
Ati <ativassileva@gmail.com>
2024-06-18 10:11:31 +0300

    Added roles seeding

---
+++ NonHateSpeechForum/NonHateSpeechForum/Infrastructure/ApplicationBuilderExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using NonHateSpeechForum.Data;
+
+namespace NonHateSpeechForum.Infrastructure
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder PrepareDatabase(this IApplicationBuilder app)
+        {
+            using var scopedServices = app.ApplicationServices.CreateScope();
+            var serviceProvider = scopedServices.ServiceProvider;
+
+            MigrateDatabase(serviceProvider);
+            SeedRoles(serviceProvider);
+
+            return app;
+        }
+
+        private static void MigrateDatabase(IServiceProvider serviceProvider)
+        {
+            var dbContext = serviceProvider
+                .GetRequiredService<ApplicationDbContext>();
+            dbContext.Database.Migrate();
+        }
+
+        private static void SeedRoles(IServiceProvider serviceProvider)
+        {
+            var dbContext = serviceProvider
+                .GetRequiredService<ApplicationDbContext>();
+
+            if (dbContext.Roles.Any())
+            {
+                return;
+            }
+
+            dbContext.Roles.AddRange(new[]
+            {
+                new IdentityRole { Name = "Regular User" },
+                new IdentityRole { Name = "Moderator" },
+                new IdentityRole { Name = "Administrator" }
+            });
+
+            dbContext.SaveChanges();
+        }
+    }
+}

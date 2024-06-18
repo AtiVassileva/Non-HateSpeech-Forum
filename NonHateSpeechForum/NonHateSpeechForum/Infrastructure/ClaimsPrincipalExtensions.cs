49970a52716025b4889fe1f10313f98e92a8a3c3
Ati <ativassileva@gmail.com>
2024-06-18 11:02:22 +0300

    Added role check extension methods

--- NonHateSpeechForum/NonHateSpeechForum/Infrastructure/ClaimsPrincipalExtensions.cs
+++ NonHateSpeechForum/NonHateSpeechForum/Infrastructure/ClaimsPrincipalExtensions.cs
@@ -6,5 +6,11 @@ namespace NonHateSpeechForum.Infrastructure
     {
         public static string GetId(this ClaimsPrincipal user)
             => user.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+
+        public static bool IsModerator(this ClaimsPrincipal user)
+            => user.IsInRole("Moderator");
+
+        public static bool IsAdmin(this ClaimsPrincipal user)
+            => user.IsInRole("Administrator");
     }
 }
